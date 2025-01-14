5e6bc22ec3c4baa1527bdec44c4da2eadf860f11
filipe-oliveiraa <filipe.amaro.oliveira.01@gmail.com>
2025-01-14 02:57:07 +0000

    Buffered channel solution | No paralelism at note sending level

--- service/conductor/baton/baton.go
+++ service/conductor/baton/baton.go
@@ -1,9 +1,11 @@
 package baton
 
 import (
+	"fmt"
 	"io"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"crossjoin.com/gorxestra/daemon/musiciand/api/client/v1"
 	"crossjoin.com/gorxestra/data"
@@ -34,12 +36,14 @@ func (b *baton) RegisterMusician(m data.Musician) error {
 	b.musicians = append(b.musicians, m)
 	return nil
 }
+
 func (b *baton) UnregisterMusician(id data.ID) error {
 	//b.mu.Lock()
 	//defer b.mu.Unlock()
 	//TODO
 	return nil
 }
+
 func (b *baton) Play(r io.Reader) error {
 	if b.playing.CompareAndSwap(false, true) {
 		go b.play(r)
@@ -50,21 +54,29 @@ func (b *baton) Play(r io.Reader) error {
 }
 
 func (b *baton) play(r io.Reader) {
-	// read and play it
-	tracks := smf.ReadTracksFrom(r)
+	defer b.playing.Store(false)
+
+	tracks := smf.ReadTracksFrom(r) //read trackes from the io.Reader
 	numTracks := len(tracks.SMF().Tracks)
 
-	channel := make(chan Note)
+	b.log.With("numTracks", numTracks).Info("Tracks parsed successfully")
 
-	trackouts := make(map[int]drivers.Out)
-	for i := range numTracks {
+	channel := make(chan Note, 1000) //Buffered channel to handle notes
+
+	trackouts := make(map[int]drivers.Out) //Store the output of channels of each track
+	for i := 0; i < numTracks; i++ {
 		trackouts[i] = &Track{
 			ch:    channel,
 			index: i,
 		}
+		b.log.With("trackIndex", i).Info("Initialized track output")
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+
 	go func() {
+		defer wg.Done()
 		for note := range channel {
 			if note.index >= len(b.musicians) {
 				b.log.
@@ -82,7 +94,13 @@ func (b *baton) play(r io.Reader) {
 				continue
 			}
 
-			b.log.With("note", note).Info("sending note")
+			b.log.With("note", fmt.Sprintf("%v", note.note)).
+				With("index", note.index).
+				With("musician_address", b.musicians[note.index].Address).
+				With("timestamp_sent", time.Now()). // Log the time when the note is sent
+				Info("Sending note to musician")
+
+			//b.log.With("note", note).Info("sending note")
 			if len(note.note) > 0 {
 				err = cli.Play(note.note)
 			}
@@ -95,13 +113,11 @@ func (b *baton) play(r io.Reader) {
 		}
 	}()
 
-	// play music
 	tracks.Do(func(ev smf.TrackEvent) {
-		b.log.Infof("track %v @%vms %s\n", ev.TrackNo, ev.AbsMicroSeconds/1000, ev.Message)
+		//b.log.Infof("track %v @%vms %s\n", ev.TrackNo, ev.AbsMicroSeconds/1000, ev.Message)
+		fmt.Printf("Track: %d | Time: %d | Message: %s\n", ev.TrackNo, ev.AbsMicroSeconds/1000, ev.Message.String())
 	}).MultiPlay(trackouts)
 
-	// close music channels
 	close(channel)
-
-	b.playing.Store(false)
+	wg.Wait()
 }
